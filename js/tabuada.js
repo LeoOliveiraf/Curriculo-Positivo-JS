6eb745b421d2b69df4a7049d261160ff00dacf8d
leoOliveiraf <oliveiraleo797@gmail.com>
2023-03-29 21:44:43 -0300

    desafios em js

---
+++ js/tabuada.js
@@ -0,0 +1,25 @@
+let cliques = 0;
+function tabuada(){
+        let number = document.getElementById("numero").value;
+        let divisoria = document.getElementById("result");
+        divisoria.innerHTML = `<p>A tabuada é ${number}</p>`;
+        for (var i = 1; i <= 10; i++) {
+          let valor = number * i;
+          const node = document.createElement("p");
+          const text = document.createTextNode(`${number} x ${i} = ${valor}`);
+          node.appendChild(text);
+          divisoria.appendChild(node);
+    }
+}
+function contador(){
+    cliques++;
+    document.getElementById("cliques").innerHTML = cliques;
+}
+function sorteio(){
+    let sorteio = document.getElementById("inicio").value;
+    let sorteioFim = document.getElementById("fim").value;
+
+    let result = Math.floor((Math.random() * (sorteioFim-sorteio +1)) + sorteio);
+    document.getElementById("aleatorio").innerHTML = result;
+}
+
